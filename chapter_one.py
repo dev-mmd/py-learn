73d62d9814dd28c2e376b350ad1120e3484a21c2
dev-mmd <tempmmd23@gmail.com>
2024-08-17 16:37:51 +0530

    chapter one added

---
+++ chapter_one.py
@@ -0,0 +1,41 @@
+# >>> mystring = "Hello world"
+# >>> mystring.
+# mystring.capitalize(    mystring.find(          mystring.isdecimal(     mystring.istitle(       mystring.partition(     mystring.rstrip(        mystring.translate(
+# mystring.casefold(      mystring.format(        mystring.isdigit(       mystring.isupper(       mystring.replace(       mystring.split(         mystring.upper(
+# mystring.center(        mystring.format_map(    mystring.isidentifier(  mystring.join(          mystring.rfind(         mystring.splitlines(    mystring.zfill(
+# mystring.count(         mystring.index(         mystring.islower(       mystring.ljust(         mystring.rindex(        mystring.startswith(
+# mystring.encode(        mystring.isalnum(       mystring.isnumeric(     mystring.lower(         mystring.rjust(         mystring.strip(
+# mystring.endswith(      mystring.isalpha(       mystring.isprintable(   mystring.lstrip(        mystring.rpartition(    mystring.swapcase(
+# mystring.expandtabs(    mystring.isascii(       mystring.isspace(       mystring.maketrans(     mystring.rsplit(        mystring.title(
+
+name = "aj"
+print(f'{name=}')
+
+# >>> not True
+# False
+# >>> not False
+# True
+# >>> True and True
+# True
+# >>> True and False
+# False
+# >>> True == 1
+# True
+# >>> False == 0
+# True
+# >>> True + True
+# 2
+# >>> False + False
+# 0
+# >>> False + True
+# 1
+# >>> True + 3
+# 4
+
+# >>> mylist = [1, 'a', 'Hello']
+# >>> for item in mylist:
+# ...     print(item)
+# ... 
+# 1
+# a
+# Hello
